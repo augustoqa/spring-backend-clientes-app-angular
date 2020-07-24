1912b69451ee1b2a52577934b12467e2fa5b8c69
Cesar Acual <augustoacual@gmail.com>
2020-07-24 12:33:32 -0600

    Escribiendo los metodos del CRUD en la clase ClienteService del Backend

---
+++ bin/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Cesar', 'Acual', 'cesar@gmail.com', '2017-12-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Mr. John', 'Doe', 'john.doe@mail.com', '2017-12-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linus', 'Torvalds', 'linus@mail.com', '2017-11-13');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Rasmus', 'Lerdorf', 'rasmus@mail.com', '2017-12-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Erich', 'Gamma', 'erich@mail.com', '2017-11-14');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Richard', 'Helm', 'richard@mail.com', '2017-10-15');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ralph', 'Johnson', 'bombasto@mail.com', '2017-11-16');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Bombasto', 'Vlissides', 'bombasto@mail.com', '2017-12-17');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Dr James', 'Gosling', 'james@mail.com', '2017-12-18');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Magma', 'Lee', 'magma@mail.com', '2017-11-19');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Tornado', 'Roe', 'tornado@mail.com', '2017-03-20');
--- src/main/java/com/ducke/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/ducke/backend/apirest/models/services/IClienteService.java
@@ -7,5 +7,11 @@ import com.ducke.backend.apirest.models.entity.Cliente;
 public interface IClienteService {
 	
 	List<Cliente> findAll();
+	
+	Cliente findById(Long id);
+	
+	Cliente save(Cliente cliente);
+	
+	void delete(Long id);
 
 }
